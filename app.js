d016a1ccac91ba28f4d7c0dd79b68848db52adea
Vitor Paulo <61637236+vitorpj20@users.noreply.github.com>
2021-03-08 08:41:29 -0300

    Arquivos carregados

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const app = express()
+const mongoose = require("mongoose")
+const bodyParser = require("body-parser")
+const cors = require("cors")
+require("dotenv/config")
+
+
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+app.use(cors())
+
+//Import routes
+const postsRoute = require("./routes/posts")
+
+app.use("/posts",postsRoute)
+
+//Routes
+app.get("/",(req,res)=>{
+    res.send("We are on home")
+})
+
+
+
+//DB
+mongoose.connect(process.env.DB_CONNECTION,{useNewUrlParser:true,useUnifiedTopology:true},()=>{console.log("Conectado no DB")})
+//Port
+app.listen(3000)
